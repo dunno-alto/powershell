8a9b88e89fe2516e7450a448297cc20c92f92d18
alto <34450932+dunno-alto@users.noreply.github.com>
2023-12-01 10:20:44 -0800

    Update ShellADDS.ps1

--- ShellADDS.ps1
+++ ShellADDS.ps1
@@ -6,7 +6,7 @@ Import-Module ADDSDeployment
 
 
 # First DC
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "eri.local" -DomainNetbiosName "CAMO-dc-02" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "domain.local" -DomainNetbiosName "dc-02" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
 
 # Additional DC  
-Install-ADDSDomainController  -DomainName "eri.local" -InstallDNS:$True -credential (Get-Credential)
+Install-ADDSDomainController  -DomainName "domain.local" -InstallDNS:$True -credential (Get-Credential)
